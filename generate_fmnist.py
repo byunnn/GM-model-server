5f0243885c73010fae56b4490cb12eb0839e0f70
haerin <byun-@naver.com>
2023-11-05 22:04:23 +0900

    Feat : add restful api

--- generate_fmnist.py
+++ generate_fmnist.py
@@ -3,24 +3,28 @@ import torch.nn as nn
 import numpy as np
 from torchvision.utils import make_grid
 from PIL import Image
-from Model.sample_model import Generator
-
+from sampleModel.sample_model import Generator
+import websockets
+import asyncio
 
 # if __name__ == "__main__":
 def generate_fmnist() :
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using {device} device")
 
-    generator_model_path = 'experiments/generator.pth'
-
+    generator_model_path = 'ckpt/generator.pth'
     # Load pretrained Generator Model
     generator = Generator().to(device) 
     generator.load_state_dict(torch.load(generator_model_path))
     generator.eval()
 
     z = torch.randn(9, 100).to(device)
+    
     labels = torch.LongTensor(np.arange(9)).to(device)
     sample_images = generator(z, labels).unsqueeze(1).data.cpu()
+
+    server_ip = "ws://192.168.170.110:8001/webSocketHandler"
+
 
     for i in range(sample_images.size(0)):
         image = sample_images[i].squeeze().numpy() 
@@ -31,4 +35,36 @@ def generate_fmnist() :
         image_path = f'images/sample_image_{i}.png'
         image.save(image_path)
 
-    print(f'{sample_images.size(0)} images saved.')
+        print(f'{sample_images.size(0)} images saved.')
+
+        asyncio.create_task(submitForm(sample_images.size(0), i))
+
+
+
+async def submitForm(total, now):
+    server_ip = "ws://192.168.170.110:8001/webSocketHandler"
+    try:
+        async with websockets.connect(server_ip) as websocket:
+            await websocket.send(f"{total} 중 {now}번째 실행 중")
+            websocket.close()
+    except Exception as e:
+        print(f"WebSocket 연결 중 오류 발생: {e}")
+
+
+
+
+# async def connect_websocket(server_ip):
+#     try:
+#         websocket = await websockets.connect(server_ip)
+#     except Exception as e:
+#         print(f"WebSocket 연결 중 오류 발생: {e}")
+#         return None
+#     return websocket
+
+
+# async def send_message(ws : websockets, total, now):
+#     try:
+#         async with ws as websocket:
+#             await ws.send(str(total) + f' 중 {now}번째 실행 중')
+#     except Exception as e:
+#         print(f"메시지 전송 중 오류 발생: {e}")
--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 from fastapi import FastAPI, File, UploadFile
-import subprocess
 import aiohttp
 import os
 import requests
-import tempfile
 import zipfile
-import logging
 import httpx
 from fastapi.responses import FileResponse
 from zipfile import ZipFile
 from generate_fmnist import generate_fmnist
-from google.cloud import storage
+from generate_gm import generate_gm
+from generate_gm import generate_main
 from fastapi import HTTPException, Depends
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import argparse 
 import asyncio
 
 app = FastAPI()
 
-server_ip = os.environ["SERVER_API_URL"]
+# server_ip = os.environ["SERVER_API_URL"]
+# server_ip = "http://172.20.10.9:8001/"
+server_ip = "http://192.168.170.110:8001/"
 
 @app.get("/")
 async def root():
@@ -35,22 +36,23 @@ async def root():
 
 
 
-class Item(BaseModel):
-    zip: str
+class File(BaseModel):
+    email : str
+    projectName : str
+    zipUrl: str
 
 
 # 클라우드 zip 파일 URL 가져오기
-@app.get("/get/url")
-async def download_and_extract(item: Item):
-    print(item.zip)
+@app.post("/get/url")
+async def download_and_extract(item: File):
+    print( item.email, ' :' ,item.zipUrl)
 
-    zip_url = 'generated_images.zip'
-    target_dir = 'dataset_from_server'
+    target_dir = 'user_dataset'
 
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
     # HTTP GET 요청을 보내 파일 다운로드
-    response = requests.get(zip_url)
+    response = requests.get(item.zipUrl)
     if response.status_code != 200:
         raise Exception("Can't download a file.")
     
@@ -58,135 +60,168 @@ async def download_and_extract(item: Item):
     file_path = os.path.join(target_dir, 'generated_images.zip')
     # 파일 저장
     with open(file_path, "wb") as f:
-        # f.write(response.content)
-        f.write(zip_url)
+        f.write(response.content)
+        # f.write(item.zip)
 
     # 압축 해제
     with ZipFile(file_path, "r") as zip_ref:
         zip_ref.extractall(target_dir)
 
+    generate_images(item.projectName, item.email)
 
-    return {"zip" : item}
 
 
-#이미지 생성 & zip 파일로 저장
-@app.get("/generate")
-async def generate_images(zip_url : str):
-
+def generate_images(projectName, email):
     try:
-        # Create the "images" folder if it doesn't exist
-        os.makedirs("images", exist_ok=True)
-
         generate_fmnist()
 
         images_folder = "images"
-        zip_filename = "generated_images.zip"
+        zip_filename = projectName+'.zip'
+        # zip_filename = "generated_images.zip"
 
         with ZipFile(zip_filename, 'w') as zipf:
             for root, _, files in os.walk(images_folder):
+                print("zip 파일 생성중")
                 for file in files:
                     file_path = os.path.join(root, file)
                     zipf.write(file_path, os.path.relpath(file_path, images_folder))
+        print("zip file 생성 완료")
+        send_zip_fun(projectName, email)
 
     except Exception as e:
         return f"An error occurred: {str(e)}"
     
     return {"message": "done", "zip_filename": zip_filename}
 
-
-    
-@app.post("/send-zip-file")
-async def send_zip_fun():
-    zip_file_path = "generated_images.zip"
-    endpoint = server_ip + "return/zipFile"  
+def send_zip_fun(projectName, email):
+    zip_file_path = projectName+'.zip'
+    endpoint = server_ip + "zips"
+    print(endpoint)
     data = {
-        "accuracy": [3.5, 5.8, 85.2, 45.3]
+        "accuracy": [3.5, 5.8, 85.2],
+        "email": email,
+        "projectName": projectName
     }
-    async with httpx.AsyncClient() as client:
-        with open(zip_file_path, "rb") as file_data:
-            response = await client.post(endpoint, files={"zipFile": (file_data)}, data = data)
+    print(data)
+    with open(zip_file_path, "rb") as file_data:
+        response = requests.post(endpoint, files={"zipFile": (file_data)}, data=data)
+        print(response)
+    
     if response.status_code == 200:
         return {"message": "Zip 파일을 성공적으로 보냈습니다."}
     else:
         return {"message": "Zip 파일 전송에 실패했습니다."}
 
-#
-
-# class FileRequest(BaseModel):
-#     zipFile: UploadFile
-
-# @app.post("/send-zip-file")
-# async def get_diary():
+# #생성된 zip파일 서버로 전송
+# async def send_zip_fun(projectName, email):
 #     zip_file_path = "generated_images.zip"
-#     endpoint = "http://172.20.10.9:8080/return/zipFile"  # 대상 서버 URL
-#     logging.info("확인")
+#     endpoint = server_ip + "zips"  
+#     data = {
+#         "accuracy": [3.5, 5.8, 85.2],
+#         "email": email,
+#         "projectName": projectName
+#     }
 #     async with httpx.AsyncClient() as client:
 #         with open(zip_file_path, "rb") as file_data:
-#             print("파일 읽음")
-#             # response = await client.get(endpoint, files={"zipFile": (file_data)})
-#             print("3")
-
-#             response = FileRequest(zipFile=file_data)
-#     print("response 확인", response)
-#     # Send the response to /model/inference/{diary_id} endpoint
-#     requests.post(endpoint, response)
+#             response = await client.post(endpoint, files={"zipFile": (file_data)}, data = data)
+#     if response.status_code == 200:
+#         return {"message": "Zip 파일을 성공적으로 보냈습니다."}
+#     else:
+#         return {"message": "Zip 파일 전송에 실패했습니다."}
 
 
+# #생성된 zip파일 서버로 전송
+# @app.post("/zips")
+# async def send_zip_fun():
+#     zip_file_path = "generated_images.zip"
+#     endpoint = server_ip + "zips"  
+#     data = {
+#         "accuracy": [3.5, 5.8, 85.2],
+#         "email" : "test@gmail.com",
+#         "projectName" : "project 1"
+#     }
+#     async with httpx.AsyncClient() as client:
+#         with open(zip_file_path, "rb") as file_data:
+#             response = await client.post(endpoint, files={"zipFile": (file_data)}, data = data)
+#     if response.status_code == 200:
+#         return {"message": "Zip 파일을 성공적으로 보냈습니다."}
+#     else:
+#         return {"message": "Zip 파일 전송에 실패했습니다."}
 
-def generate_images():
-    try:
-        generate_fmnist()
 
-        images_folder = "images"
-        zip_filename = "generated_images.zip"
 
-        with ZipFile(zip_filename, 'w') as zipf:
-            for root, _, files in os.walk(images_folder):
-                for file in files:
-                    file_path = os.path.join(root, file)
-                    zipf.write(file_path, os.path.relpath(file_path, images_folder))
 
-    except Exception as e:
-        return f"An error occurred: {str(e)}"
+
+# def sample_demo():
+
+
+#     gm_args = {
+#         "mode": "eval",
+#         "num_domains": 3,
+#         "w_hpf": 0,
+#         "resume_iter": 5000,
+#         "train_img_dir": "data/x-raymed/train",
+#         "val_img_dir": "data/x-raymed/val",
+#         "checkpoint_dir": "expr/checkpoints/x-raymed",
+#         "eval_dir": "expr/eval/x-raymed"
+#     }
+
+#     # combined_args = {**gm_args, **args}
+
+#     # print("///////////////////////////////////////1", args)
+#     # print("///////////////////////////////////////", combined_args) 
+
+#     generate_main(gm_args)
+
+
+# #이미지 생성 & zip 파일로 저장
+# @app.get("generate")
+# async def generate_images(zip_url : str):
+
+#     try:
+#         # Create the "images" folder if it doesn't exist
+#         os.makedirs("images", exist_ok=True)
+
+#         gm_args = {
+#             "mode": "eval",
+#             "num_domains": 3,
+#             "w_hpf": 0,
+#             "resume_iter": 5000,
+#             "train_img_dir": "data/x-raymed/train",
+#             "val_img_dir": "data/x-raymed/val",
+#             "checkpoint_dir": "expr/checkpoints/x-raymed",
+#             "eval_dir": "expr/eval/x-raymed"
+#         }
+
+#         generate_gm(gm_args) 
+
+
+#         images_folder = "images"
+#         zip_filename = "generated_images.zip"
+
+#         with ZipFile(zip_filename, 'w') as zipf:
+#             for root, _, files in os.walk(images_folder):
+#                 for file in files:
+#                     file_path = os.path.join(root, file)
+#                     zipf.write(file_path, os.path.relpath(file_path, images_folder))
+
+#     except Exception as e:
+#         return f"An error occurred: {str(e)}"
     
-    return {"message": "done", "zip_filename": zip_filename}
-
+#     return {"message": "done", "zip_filename": zip_filename}
 
 # if __name__ == '__main__':
-#     send_zip_fun()
-    # generate_images()
-    # 다운로드할 파일의 URL 및 로컬 디렉토리 설정
-    # url = "https://storage.googleapis.com/gm-medical-project/gm-zip.zip-6b3e9cc1-e545-4e29-b47a-82a0f0c4fc69"
-    # g_url = "https://drive.google.com/file/d/1Fw4ytuJbKBWsPuHAz3foTb1ZhTG702Sa/view?usp=drive_link"
-    # target_dir = "images"
-    # download_and_extract(g_url)
-    # download_and_extract_zip(url, target_dir)
-    # 파일 다운로드 및 압축 해제 함수 호출
-    # download_and_extract(url, target_dir)
-
-
-
-
-# async def download_and_extract_url(url: str):
-#     target_dir = 'images'
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(url) as response:
-#             if response.status != 200:
-#                 raise HTTPException(status_code=404, detail="Can't download a file.")
-            
-#             temp_dir = tempfile.mkdtemp()
-#             temp_file_path = os.path.join(temp_dir, "downloaded_file.zip")
-
-#             with open(temp_file_path, "wb") as f:
-#                 while True:
-#                     chunk = await response.content.read(1024)
-#                     if not chunk:
-#                         break
-#                     f.write(chunk)
-#             with ZipFile(temp_file_path, "r") as zip_ref:
-#                 zip_ref.extractall(target_dir)
-#             os.remove(temp_file_path)
-#             os.rmdir(temp_dir)
+#     sample_demo()
+# #     send_zip_fun()
+#     # generate_images()
+#     # 다운로드할 파일의 URL 및 로컬 디렉토리 설정
+#     # url = "https://storage.googleapis.com/gm-medical-project/gm-zip.zip-6b3e9cc1-e545-4e29-b47a-82a0f0c4fc69"
+#     # g_url = "https://drive.google.com/file/d/1Fw4ytuJbKBWsPuHAz3foTb1ZhTG702Sa/view?usp=drive_link"
+#     # target_dir = "images"
+#     # download_and_extract(g_url)
+#     # download_and_extract_zip(url, target_dir)
+#     # 파일 다운로드 및 압축 해제 함수 호출
+#     # download_and_extract(url, target_dir)
 
 
 
@@ -195,26 +230,6 @@ def generate_images():
 
 
 
-# def download_and_extract_zip(url, target_dir):
 
-#     # 파일 다운로드
-#     response = requests.get(url)
-#     if response.status_code != 200:
-#         raise Exception("Can't download a file.")
-    
-#     # 임시 디렉토리에서 작업
-#     temp_dir = tempfile.mkdtemp()
-#     temp_file_path = os.path.join(temp_dir, "downloaded_file.zip")
 
-#     with open(temp_file_path, "wb") as f:
-#         f.write(response.content)
 
-#     # 만약 파일이 zip 파일이면 압축 해제
-#     if temp_file_path.endswith(".zip"):
-#         zip_dir = os.path.join(temp_dir, "extracted")
-#         with zipfile.ZipFile(target_dir, "r") as zip_ref:
-#             zip_ref.extractall(zip_dir)
-    
-#     # 임시 디렉토리 정리s
-#     os.remove(temp_file_path)
-#     os.rmdir(temp_dir)
